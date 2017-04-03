5ae2a596156fa9c51879ce6784c7bfa5a96d1a8f
Strunk <12001003134353@FEDIDCARD.GOV>
2017-04-03 15:30:03 -0700

    debug

--- examples/tile_project_example.R
+++ examples/tile_project_example.R
@@ -17,6 +17,8 @@ tile_project(
   ,crs="+proj=lcc +lat_1=47.33333333333334 +lat_2=45.83333333333334 +lat_0=45.33333333333334 +lon_0=-120.5 +x_0=500000.0001016001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs"
 )
 
+library(rgdal)
+proj_area=readOGR(dsn="C:\\projects\\2017_WA_DSM_Pilot",layer="extent")
 
 tile_project(
   dir_las="G:\\phodar\\NAIP_2015\\las_files\\"
@@ -27,7 +29,7 @@ tile_project(
   ,project_las="naip"
   ,dtm_year="2015"
   ,las_year="2015"
-  ,mask=NA
+  ,mask=proj_area
   ,scan_dtms=T
   ,scan_las=T
   ,create_project=T
