49feebbdc4a3cc7538ca8eafb19f27d2df251c13
Strunk <12001003134353@FEDIDCARD.GOV>
2017-05-18 14:26:48 -0700

    fix clip plots

--- R/clip_plots.R
+++ R/clip_plots.R
@@ -37,7 +37,7 @@ clip_plots=function(
     if(!inherits(lasR_project_polys,"sp")) proj_polys=readOGR(dirname(lasR_project_polys),basename(lasR_project_polys))
     if(inherits(lasR_project_polys,"sp")) proj_polys=lasR_project_polys
   }
-print("load lasR_project");print(Sys.time())
+  print("load lasR_project");print(Sys.time())
   #fix drive paths in lasR_project
   if(!is.na(dir_dtm)) proj_polys@data[,"dtm_file"]=unlist(lapply(proj_polys@data[,"dtm_file"],function(...,dir_dtm)paste(file.path(dir_dtm,basename(strsplit(...,",")[[1]])),collapse=","),dir_dtm=dir_dtm))
   if(!is.na(dir_las)) proj_polys@data[,"las_file"]=unlist(lapply(proj_polys@data[,"las_file"],function(...,dir_dtm)paste(file.path(dir_dtm,basename(strsplit(...,",")[[1]])),collapse=","),dir_dtm=dir_las))
@@ -48,10 +48,10 @@ print("load lasR_project");print(Sys.time())
     if(inherits(plot_polys,"sp")) polys_in=plot_polys
     if(inherits(plot_polys,"char")) polys_in=readOGR(plot_polys)
   }
-  if(!is.na(idxy[1]) & !inherits(polys_in,"sp")){
+  if(!is.na(unlist(idxy)[1]) & !inherits(polys_in,"sp")){
     polys_in=points2polys(idxy)
   }
-  if(!is.na(idxyd[1]) & !inherits(polys_in,"sp")){
+  if(!is.na(unlist(idxyd)[1]) & !inherits(polys_in,"sp")){
 
     seq1=c(seq(-pi,pi,.1),pi+.1)
     circle1=data.frame(sin(seq1),cos(seq1))
@@ -63,7 +63,7 @@ print("load lasR_project");print(Sys.time())
     plot_polys=SpatialPolygonsDataFrame(plot_polys_0,data=idxyd)
 
   }
-print("Create Plot Polys");print(Sys.time())
+  print("Create Plot Polys");print(Sys.time())
 
   #clean up self intersections
   proj_polys_b=gBuffer(proj_polys, byid=TRUE, width=0)
@@ -86,13 +86,13 @@ print("Create Plot Polys");print(Sys.time())
   plot_polys_spdf=SpatialPolygonsDataFrame(plot_polys_ext,plot_polys_b1@data)
   proj_polys_spdf=SpatialPolygonsDataFrame(proj_polys_ext,proj_polys_b1@data)
 
-print("clip, recombine plots with data");print(Sys.time())
+  print("clip, recombine plots with data");print(Sys.time())
 
   #intersect plots with tiles
   proj_plot_x=gIntersects(plot_polys_spdf,proj_polys_spdf, byid=T,returnDense=F)
   proj_plot_x1=proj_plot_x[sapply(proj_plot_x,function(x)length(x)>0)]
 
-print("intersect plots and tiles");print(Sys.time())
+  print("intersect plots and tiles");print(Sys.time())
 
   #parse intersections and compile data
   plots_tiles=rbind.fill(mapply(function(x,y,plots,tiles){data.frame(plots[as.character(x),],tiles[y,],row.names=NULL)},names(proj_plot_x1),proj_plot_x1,SIMPLIFY = F, MoreArgs = list(plots=plot_polys_spdf@data,tiles=proj_polys_spdf@data)))
@@ -106,10 +106,11 @@ print("intersect plots and tiles");print(Sys.time())
   plots_tiles_unq=rbind(no_dups_df,dups_df)
   row.names(plots_tiles_unq)=plots_tiles_unq[,1]
 
-print("merge duplicates");print(Sys.time())
-browser()
+  print("merge duplicates");print(Sys.time())
+
   #add records to geometry
-  plot_polys_merge=SpatialPolygonsDataFrame(plot_polys_ext[names(plot_polys_ext) %in% (plots_tiles_unq[,1])],plots_tiles_unq)
+  good_polys=names(plot_polys_ext) %in% (plots_tiles_unq[,1])
+  plot_polys_merge=SpatialPolygonsDataFrame(plot_polys_ext[good_polys,],plots_tiles_unq)
 
   if(do_plot){
     plot(proj_polys_spdf)
@@ -117,7 +118,7 @@ browser()
   }
 
   #clip points
-  spl_plots=split(plot_polys_merge,1:nrow(plot_polys_merge))
+  spl_plots=split(plot_polys_merge,1:nrow(plot_polys_merge),drop=T)
 
   if(n_core>1){
     clus=makeCluster(n_core)
@@ -129,14 +130,14 @@ browser()
   }
 
   #.try_clip_plots(spl_plots[[2]],dir_out = dir_out)
-print("clip plots");print(Sys.time())
+  print("clip plots");print(Sys.time())
 
   #write shapefile of intersections
   dir_overlap=file.path(dir_out,"plot_tile_overlap")
   if(!dir.exists(dir_overlap)) dir.create(dir_overlap)
   writeOGR(plot_polys_merge,dir_overlap,"plot_merge_tiles", driver="ESRI Shapefile")
 
-print("write outputs");print(Sys.time())
+  print("write outputs");print(Sys.time())
 
   if(return) return(plot_polys_merge)
 
@@ -236,40 +237,3 @@ print("write outputs");print(Sys.time())
 #
 # }
 
-if(F){
-
-  library(rgdal)
-  library(lasR)
-  plxy=readOGR("C:\\projects\\2017_WA_DSM_Pilot\\plots","fia_plots_fuzz")
-  proj4string(plxy)
-  projTo="+proj=lcc +lat_1=47.33333333333334 +lat_2=45.83333333333334 +lat_0=45.33333333333334 +lon_0=-120.5 +x_0=500000.0001016001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs"
-  plxy1=spTransform(plxy,projTo)
-  plxy1@data[,c("x","y")]=coordinates(plxy1)
-  idxyd1=data.frame(plxy1@data[,c("PLOT","x","y")],d=24)
-  names(idxyd1)=tolower(names(idxyd1))
-  idxyd2=idxyd1[!duplicated(idxyd1$plot),]
-
-  clip_plots(lasR_project="C:\\projects\\2017_WA_DSM_Pilot\\DSM_Pilot_5cnty_lasR\\lasR_project001.csv"
-             ,idxyd=idxyd2
-             ,dir_out="C:\\projects\\2017_WA_DSM_Pilot\\DSM_Pilot_5cnty_lasR\\plot_clips"
-             #fix bad directory:
-             ,dir_las="I:\\phodar\\NAIP_2015\\las_files\\"
-             #fix bad directory:
-             ,dir_dtm="H:\\DNR\\FUSION_DTMS\\"
-             ,n_core=8
-
-  )
-
-  clip_plots(lasR_project="C:\\projects\\2017_WA_DSM_Pilot\\DSM_Pilot_5cnty_lasR\\lasR_project001.csv"
-             ,idxyd=idxyd2
-             ,dir_out="C:\\projects\\2017_WA_DSM_Pilot\\DSM_Pilot_5cnty_lasR\\plot_clips_elev"
-             #fix bad directory:
-             ,dir_las="I:\\phodar\\NAIP_2015\\las_files\\"
-             #fix bad directory:
-             ,dir_dtm="H:\\DNR\\FUSION_DTMS\\"
-             ,n_core=7
-             ,height=F
-
-  )
-
-}
