9de528810e0be802692a6f0a0530375b6c7dd2ab
Strunk <12001003134353@FEDIDCARD.GOV>
2018-03-05 11:07:27 -0800

    write_dtm

--- R/write_dtm.R
+++ R/write_dtm.R
@@ -110,22 +110,22 @@ write_dtm=function(
   }
 
 
-if(F){
-
-
-  dtm1=read_dtm("C:\\Temp\\47_62.dtm")
-  hd1=read_dtm_header("C:\\Temp\\47_62.dtm")
-  write_dtm(dtm=dtm1,dir_out="c:\\temp\\47_62_b.dtm",zfmt=3)
-  write_dtm(dtm=dtm1,dir_out="c:\\temp\\47_62_b.dtm",zfmt=0)
-
-  dtm2=read_dtm("C:\\Temp\\47_62_b.dtm")
-
-
-  hd2=read_dtm_header("C:\\Temp\\47_62_b.dtm")
-  eqs = dtm1[]==dtm2[]
-  sum(is.na(dtm1[]))
-  sum(is.na(dtm2[]))
-  eqs[is.na()]
-  boxplot(eqs)
-
-}
+# if(F){
+#
+#
+#   dtm1=read_dtm("C:\\Temp\\47_62.dtm")
+#   hd1=read_dtm_header("C:\\Temp\\47_62.dtm")
+#   write_dtm(dtm=dtm1,dir_out="c:\\temp\\47_62_b.dtm",zfmt=3)
+#   write_dtm(dtm=dtm1,dir_out="c:\\temp\\47_62_b.dtm",zfmt=0)
+#
+#   dtm2=read_dtm("C:\\Temp\\47_62_b.dtm")
+#
+#
+#   hd2=read_dtm_header("C:\\Temp\\47_62_b.dtm")
+#   eqs = dtm1[]==dtm2[]
+#   sum(is.na(dtm1[]))
+#   sum(is.na(dtm2[]))
+#   eqs[is.na()]
+#   boxplot(eqs)
+#
+# }
